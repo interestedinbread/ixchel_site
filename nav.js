7e06a8c6605912db5d2d40494f4ac8cddda48474
Eric Morrison <eric_m250@outlook.com>
2025-03-26 13:21:00 -0700

    added some simple fixes for loose scrolling on mobile

--- nav.js
+++ nav.js
@@ -1,45 +1,50 @@
 export const setupNavlinks = () => {
-    const navlinks = document.querySelectorAll('.nav-link')
-    const navlinksContainer = document.querySelector('.nav-links')
-    const menuBtn = document.querySelector(".menu-icon");
-    const navHeight = document.querySelector('nav').getBoundingClientRect().height;
-    const ixchelLogoContainer = document.querySelector('.ixchel-logo-container');
-    const toggleBtn = document.querySelector('.menu-icon');
-    const navElement = document.querySelector('nav');
+  const navlinks = document.querySelectorAll(".nav-link");
+  const navlinksContainer = document.querySelector(".nav-links");
+  const menuBtn = document.querySelector(".menu-icon");
+  const navHeight = document
+    .querySelector("nav")
+    .getBoundingClientRect().height;
+  const ixchelLogoContainer = document.querySelector(".ixchel-logo-container");
+  const toggleBtn = document.querySelector(".menu-icon");
+  const navElement = document.querySelector("nav");
 
-    navlinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            toggleNav();
-            const id = e.target.id.slice(1)
+  navlinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      toggleNav();
+      const id = e.target.id.slice(1);
 
-            const targetElement = document.getElementById(id);
-            let position = targetElement.offsetTop - navHeight;
-            window.scrollTo({
-                left: 0,
-                top: position
-            })
-        })
-    })
+      const targetElement = document.getElementById(id);
+      let position = targetElement.offsetTop - navHeight;
+      window.scrollTo({
+        left: 0,
+        top: position,
+      });
+    });
+  });
 
-    const toggleNav = () => {
-        navlinksContainer.classList.toggle("active");
-        menuBtn.classList.toggle("active");
-      };
-    
-    menuBtn.addEventListener("click", toggleNav);
+  const toggleNav = () => {
+    navlinksContainer.classList.toggle("active");
+    menuBtn.classList.toggle("active");
+  };
 
-    ixchelLogoContainer.addEventListener('click', () => {
-        window.scrollTo({
-            left: 0,
-            top: 0
-        });
-    })
+  menuBtn.addEventListener("click", toggleNav);
 
-    document.addEventListener('click', (e) => {
-        if(!navElement.contains(e.target) && !toggleBtn.contains(e.target)){
-            toggleNav();
-        }
-    })
-}
+  ixchelLogoContainer.addEventListener("click", () => {
+    window.scrollTo({
+      left: 0,
+      top: 0,
+    });
+  });
 
+  document.addEventListener("click", (e) => {
+    if (
+      !navElement.contains(e.target) &&
+      !toggleBtn.contains(e.target) &&
+      navlinksContainer.classList.contains("active")
+    ) {
+      toggleNav();
+    }
+  });
+};
