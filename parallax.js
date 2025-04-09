e0418e3d901dd26d404c4fefefd37871213b0460
Eric Morrison <eric_m250@outlook.com>
2025-04-09 10:45:08 -0700

    removed cable bay img at bottom of page. Increased height of parallax elements.

--- parallax.js
+++ parallax.js
@@ -2,7 +2,7 @@ export const setupParallax = () => {
     const backTrees = document.querySelector('.layer-1')
     const frontTrees = document.querySelector('.layer-2')
 
-    const triggerOffset = 500;
+    const triggerOffset = 800;
 
     window.addEventListener('scroll', () => {
         const scrollTop = window.scrollY;
@@ -13,8 +13,8 @@ export const setupParallax = () => {
 
         if(scrollFromBottom <= triggerOffset){
             const progress = 1 - scrollFromBottom/triggerOffset;
-            backTrees.style.transform = `translateY(${progress * 30}px)`;
-            frontTrees.style.transform = `translateY(${progress * 60}px)`;
+            backTrees.style.transform = `translateY(${progress * 60}px)`;
+            frontTrees.style.transform = `translateY(${progress * 120}px)`;
         } else {
             backTrees.style.transform = `translateY(0)`
             frontTrees.style.transform = `translateY(0)`
