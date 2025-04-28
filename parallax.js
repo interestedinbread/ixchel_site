46665ed02ab336a8afda424914b21f3d43cf6519
Eric Morrison <eric_m250@outlook.com>
2025-04-28 15:27:26 -0700

    fixed parallax

--- parallax.js
+++ parallax.js
@@ -2,7 +2,10 @@ export const setupParallax = () => {
     const backTrees = document.querySelector('.layer-1')
     const frontTrees = document.querySelector('.layer-2')
 
+    // Start the effect when the scene is about 30% into view
     const triggerOffset = 800;
+    const maxBackMovement = 200; // Increased movement
+    const maxFrontMovement = 400; // Increased movement
 
     window.addEventListener('scroll', () => {
         const scrollTop = window.scrollY;
@@ -13,8 +16,8 @@ export const setupParallax = () => {
 
         if(scrollFromBottom <= triggerOffset){
             const progress = 1 - scrollFromBottom/triggerOffset;
-            backTrees.style.transform = `translateY(${progress * 60}px)`;
-            frontTrees.style.transform = `translateY(${progress * 120}px)`;
+            backTrees.style.transform = `translateY(${progress * maxBackMovement}px)`;
+            frontTrees.style.transform = `translateY(${progress * maxFrontMovement}px)`;
         } else {
             backTrees.style.transform = `translateY(0)`;
             frontTrees.style.transform = `translateY(0)`;
